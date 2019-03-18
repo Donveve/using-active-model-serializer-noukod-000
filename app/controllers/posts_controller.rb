244759d1e77584a6193046daf7064020b661fda5
Veniel Dorestave <veniel@noukod.com>
2019-03-18 14:11:26 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,10 +9,10 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
       #BEFORE using a serializer:
-    #  render json: @post.to_json(only: [:title, :description, :id],
-                              #  include: [author: { only: [:name]}])
+      # render json: @post.to_json(only: [:title, :description, :id],
+      #                           include: [author: { only: [:name]}])
        # AFTER USING OUR SERIALIZER
-       render json: @post, status: 200
+      render json: @post, status: 200
   end
 
   def new
@@ -34,10 +34,12 @@ class PostsController < ApplicationController
   end
 
 private
+  # Use callbacks to share common setup or constraints between actions.
   def set_post
     @post = Post.find(params[:id])
   end
 
+  # Never trust parameters from the scary internet, only allow the white list through.
   def post_params
     params.require(:post).permit(:title, :description)
   end
